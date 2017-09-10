c428b9403f72edf9a8606a201840a5f7387d1ea1
lud77 <stefano.butera@nethnic.com>
2017-09-10 20:01:47 +0100

    Remove a custom function already provided by Ramda

--- src/domain/palindrome.js
+++ src/domain/palindrome.js
@@ -1,19 +1,16 @@
+const R = require('ramda');
+
 /**
  * Return a string with all non-alphanumeric chars removed
  */
 const stripNonAlphanumericChars = (sequence) => sequence.replace(/[^A-Za-z0-9]/g, '');
 
 /**
- * Return the reverse of a sequence of chars
- */
-const reverseSequence = (sequence) => sequence.split('').reverse().join('');
-
-/**
  * Check whether a sentence is palindromic or not, regardless of case, spaces, and punctuation
  */
 const isPalindrome = (sentence) => {
   const regularisedSentence = stripNonAlphanumericChars(sentence || '').toLowerCase();
-  const reversedSentence = reverseSequence(regularisedSentence);
+  const reversedSentence = R.reverse(regularisedSentence);
   return regularisedSentence === reversedSentence;
 };
 
