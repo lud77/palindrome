78fea94d26656d216ae043fe89c79aad9f3b1f52
lud77 <stefano.butera@nethnic.com>
2017-09-10 14:08:22 +0100

    Add test cases for isPalindrome domain method

---
+++ test/domain.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const { isPalindrome } = require('../src/domain/palindrome');
+
+describe('domain methods', () => {
+  describe('isPalindrome', () => {
+    it('should return true for a palindromic sentence regardless of case, spaces and punctuation', () => {
+      assert.isOk(isPalindrome('Noel sees Leon.'));
+      assert.isOk(isPalindrome('Too far, Edna, wander afoot.'));
+    });
+
+    it('should return false for a non palindromic sentence', () => {
+      assert.isNotOk(isPalindrome('noel sees a lion'));
+      assert.isNotOk(isPalindrome('...abc...'));
+    });
+  });
+});
