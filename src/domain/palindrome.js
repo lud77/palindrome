923c07d88d96ed9f4b89d51f44da4bb81a0dca33
lud77 <stefano.butera@nethnic.com>
2017-09-10 14:25:19 +0100

    Add implementation for the isPalindrome domain method

--- src/domain/palindrome.js
+++ src/domain/palindrome.js
@@ -1,5 +1,20 @@
-const isPalindrome = () => {
-  return true;
+/**
+ * Return a string with all non-alphanumeric chars removed
+ */
+const stripNonAlphanumericChars = (sequence) => sequence.replace(/[^A-Za-z0-9]/g, '');
+
+/**
+ * Return the reverse of a sequence of chars
+ */
+const reverseSequence = (sequence) => sequence.split('').reverse().join('');
+
+/**
+ * Check whether a sentence is palindromic or not, regardless of case, spaces, and punctuation
+ */
+const isPalindrome = (sentence) => {
+  const regularisedSentence = stripNonAlphanumericChars(sentence || '').toLowerCase();
+  const reversedSentence = reverseSequence(regularisedSentence);
+  return regularisedSentence === reversedSentence;
 };
 
 module.exports = {
