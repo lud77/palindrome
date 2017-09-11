d028fb15a8c6584d614fb35a8b95505d39389822
lud77 <stefano.butera@nethnic.com>
2017-09-11 16:22:43 +0100

    Remove useless comment

--- src/domain/palindrome.js
+++ src/domain/palindrome.js
@@ -1,8 +1,5 @@
 const R = require('ramda');
 
-/**
- * Return a string with all non-alphanumeric chars removed
- */
 const stripNonAlphanumericChars = (sequence) => sequence.replace(/[^A-Za-z0-9]/g, '');
 
 /**
