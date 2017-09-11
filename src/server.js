1f67dfd256fc670bfee9ce60b350d3ecfc08ea02
lud77 <stefano.butera@nethnic.com>
2017-09-11 16:07:15 +0100

    Add helmet, winston, body-parser to the server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const logger = require('winston');
+const expressWinston = require('express-winston');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
+
+app.disable('etag');
+app.use(helmet());
+
+app.use(bodyParser.json({
+  type: 'application/json'
+}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// request logger
+app.use(expressWinston.logger({
+  transports: [
+    new logger.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ]
+}));
+
+// error logger
+app.use(expressWinston.errorLogger({
+  transports: [
+    new logger.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ]
+}));
 
 const server = app.listen(process.env.PORT || 29292, () => {
   logger.info('Listening on port ' + server.address().port);
